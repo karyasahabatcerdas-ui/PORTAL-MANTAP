ae1acc1440db7a50af5daf6d8b1db0ddc1feefb3
nofadin <nofadin@gmail.com>
2026-02-27 12:47:03 +0700

    cek, setting untuk PWA

--- mantap-1/sw.js
+++ mantap-1/sw.js
@@ -1,10 +1,12 @@
 const CACHE_NAME = 'mantap1-ksc-v1';
 const urlsToCache = [
-  '/mantap-1/',
-  '/mantap-1/index.html',
-  '/mantap-1/manifest.json',
-  '/mantap-1/icon-192.png',
-  '/mantap-1/icon-512.png'
+  "./",
+  "index.html",
+  "manifest.json",
+  "style.css",
+  "script.js",
+  "icon-192.png",
+  "icon-512.png"
 ];
 
 self.addEventListener('install', event => {
--- mantap-2/sw.js
+++ mantap-2/sw.js
@@ -1,10 +1,12 @@
 const CACHE_NAME = 'mantap2-ksc-v1';
 const urlsToCache = [
-  '/mantap-2/',
-  '/mantap-2/index.html',
-  '/mantap-2/manifest.json',
-  '/mantap-2/icon-192.png',
-  '/mantap-2/icon-512.png'
+  "./",
+  "index.html",
+  "manifest.json",
+  "style.css",
+  "script.js",
+  "icon-192.png",
+  "icon-512.png"
 ];
 
 self.addEventListener('install', event => {
